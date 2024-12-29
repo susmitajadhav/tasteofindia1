20f728722479dee438aa0b06dfa62a6b9e88c008
susmitajadhav <45027764+susmitajadhav@users.noreply.github.com>
2024-12-29 17:14:46 +0530

    Add files via upload

---
+++ assets/Banner-DlUdRuP8.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-BX1Ylhwe.js";const n=()=>e.jsx("div",{className:"banner",children:e.jsxs("div",{className:"left",children:['"Enjoy Cooking Fun.',e.jsx("br",{}),' Love Every Delicious Bite."']})});export{n as default};
